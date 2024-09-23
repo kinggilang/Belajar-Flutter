1bd8b6363141328e43fdaaadee559f5d70705225
kinggilang <bagusgilang233@gmail.com>
2024-09-23 10:50:33 +0700

    update

---
+++ lib/text widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyTextWidget extends StatelessWidget {
+  const MyTextWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    returt conts Text
+    "Nama saya gilang bagus tp",
+    style: TextStyle(color: Colors.blue, fontSize: 14),
+    textAlign: TextAlign.center)
+  }
+}
